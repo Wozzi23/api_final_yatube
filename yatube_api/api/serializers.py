192766e1819949c575c0ceb7bccd5626d1c748aa
wozzi <wozzi56@icloud.com>
2022-06-26 13:54:22 +0300

    Первое ревью v1.0

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,90 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+    """
+    Сериализатор модели Post, поле author имеент значение
+    по умолчанию в виде текущего пользователя сделавшего запрос.
+    """
+    author = serializers.StringRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
     )
 
     class Meta:
-        fields = '__all__'
+        fields = ('id',
+                  'author',
+                  'text',
+                  'pub_date',
+                  'image',
+                  'group',
+                  )
+        model = Post
+        read_only_fields = ('author',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """"
+    Сериализатор модели Grup.
+    """
+    class Meta:
+        fields = ('id', 'title', 'slug', 'description',)
+        model = Group
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """"
+    Сериализатор модели Comment, поле author имеент значение
+    по умолчанию в виде текущего пользователя сделавшего запрос,
+    поле post передается из URL запроса пользователя.
+    """
+    author = serializers.StringRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = ('id', 'author', 'text', 'created', 'post',)
         model = Comment
+        read_only_fields = ('author', 'post')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """"
+    Сериализатор модели Follow, поле user имеент значение
+    по умолчанию в виде текущего пользователя сделавшего запрос.
+    """
+    user = serializers.StringRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        fields = ('user', 'following',)
+        model = Follow
+        read_only_fields = ('user',)
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            ),
+        )
+
+    def validate_following(self, following):
+        """
+        Проверка поля following,
+        не дает в запросе POST подписаться на самого себя.
+        """
+        print(self.context.get('request').data['following'])
+        if self.context.get('request').user == following:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя.')
+        return following
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,99 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, filters, mixins
+from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import (IsAuthenticated,
+                                        AllowAny,
+                                        IsAuthenticatedOrReadOnly)
+from rest_framework.viewsets import GenericViewSet
+
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (PostSerializer,
+                             GroupSerializer,
+                             CommentSerializer,
+                             FollowSerializer)
+from posts.models import Post, Group, Comment, Follow, User
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Модель обрабатывает запросы GET от любого пользователя,
+    POST запросы доступны только зарегистрированному пользователю
+    остальные методы PUT, PATCH, DELETE доступны только автору поста,
+    реализован стандартный метод паджинации,
+    через передачу у URL limit и offset.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly & IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Модель обрабатывает только GET запросы от любого пользователя,
+    изменение данных не доступно.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (AllowAny,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Модель обрабатывает запросы GET от любого пользователя
+    POST запросы доступны только зарегистрированному пользователю
+    остальные методы PUT, PATCH, DELETE доступны только автору комментария,
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly & IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        """
+        Получаем номер поста из URL запроса и
+        этот обьект передаем в новый queryset
+        """
+        post_id = self.kwargs.get('post_id')
+        new_queryset = Comment.objects.filter(post=post_id)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        """
+        Функция сохранения комментария, пост получаем из URL запроса,
+        поле author - пользователь сделавший запрос.
+        """
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    GenericViewSet):
+    """
+    Модель подписки на авторов. Доступна только авторизованным пользователям,
+    обрабатывает GET и POST запросы, иные действия не достуны. Реазован поиск
+    по подпискам для этого необходимо в URL передать парамет search.
+    """
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        """
+        Получаем подписки пользователя сделавшего запрос
+        и этот обьект передаем в new_queryset
+        """
+        new_queryset = Follow.objects.filter(user=self.request.user)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        """
+        Функция сохранения подписки,
+        поле user - пользователь сделавший запрос.
+        """
+        following = get_object_or_404(User,
+                                      username=self.request.data['following'])
+        serializer.save(user=self.request.user, following=following)
